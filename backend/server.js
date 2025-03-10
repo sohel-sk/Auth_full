a169f170d318171251e706cb0938f8c43fc1c510
sohel-sk <sohelsheikh9623@gmail.com>
2025-03-10 23:10:54 +0530

    mongoDB Atlas connected and tailwind setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
-const express = require('express')
-const app =express()
-const port = 8000
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require("dotenv").config({path: __dirname+"/.env"});
+console.log("PORT from .env:", process.env.PORT);
+const PORT = process.env.PORT;
 
-app.get('/',(req,res) => {
-    res.send('Hello World!')
+// Initialize Express app
+const app =express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connnected!"))
+    .catch((err) => console.log(err));
+    
+// test route 
+
+app.get("/", (req, res) => {
+    console.log(" MERN Auth APi is running.. ");
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening to port ${port}`);
-})
+// start server
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
